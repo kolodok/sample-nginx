eea522a1e856f206dbc4696268a487bf74f5d53e
alefril <44679480+alefril@users.noreply.github.com>
2020-09-07 17:45:34 +0200

    Add files via upload

--- init.sh
+++ init.sh
@@ -4,5 +4,8 @@
 wget -O /code/app.zip https://raw.githubusercontent.com/kolodok/sample-nginx/master/app.zip
 #wget -O /code/rainloop.zip https://raw.githubusercontent.com/kolodok/sample-nginx/master/rainloop-community-latest.zip
 
-unzip /code/app.zip -o -d /code
+wget -O /code/phpMyAdmin.zip https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip
+
+unzip /code/app.zip -o -d /code/
+unzip /code/phpMyAdmin.zip -o -d /code/
 #unzip /code/rainloop.zip -o -d /code

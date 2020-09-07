ef3f938a135eebfa3329474a5ece69b03357e946
alefril <44679480+alefril@users.noreply.github.com>
2020-09-07 17:29:45 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget -O /code/index.php https://raw.githubusercontent.com/do-community/php-kubernetes/master/index.php
-#wget -O /code/app.zip https://raw.githubusercontent.com/kolodok/sample-nginx/master/app.zip
-wget -O /code/rainloop.zip https://raw.githubusercontent.com/kolodok/sample-nginx/master/rainloop-community-latest.zip
+#wget -O /code/index.php https://raw.githubusercontent.com/do-community/php-kubernetes/master/index.php
+wget -O /code/app.zip https://raw.githubusercontent.com/kolodok/sample-nginx/master/app.zip
+#wget -O /code/rainloop.zip https://raw.githubusercontent.com/kolodok/sample-nginx/master/rainloop-community-latest.zip
 
-#unzip /code/app.zip -o -d /code/app
-unzip /code/rainloop.zip -o -d /code
+unzip /code/app.zip -o -d /code
+#unzip /code/rainloop.zip -o -d /code

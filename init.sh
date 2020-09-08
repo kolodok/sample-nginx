c7ca0c9de6facb57eae651fc2cb2b108070963da
alefril <44679480+alefril@users.noreply.github.com>
2020-09-08 17:36:13 +0200

    Add files via upload

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#wget -O /code/index.php https://raw.githubusercontent.com/do-community/php-kubernetes/master/index.php
+wget -O /code/index.php https://raw.githubusercontent.com/do-community/php-kubernetes/master/index.php
 #wget -O /code/app.zip https://raw.githubusercontent.com/kolodok/sample-nginx/master/app.zip
 #wget -O /code/rainloop.zip https://raw.githubusercontent.com/kolodok/sample-nginx/master/rainloop-community-latest.zip
 
